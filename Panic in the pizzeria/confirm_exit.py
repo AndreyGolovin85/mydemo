2183eba16d5d5db450b73a501673165731da62ea
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2019-02-24 21:25:06 +0300

    Update confirm_exit.py

--- Panic in the pizzeria/confirm_exit.py
+++ Panic in the pizzeria/confirm_exit.py
@@ -1,8 +1,18 @@
 import sys
 import pygame
 
-class run_confirm_exit():
-    def __init__(self, ai_setting, screen_game)
-    self.screen_game = pygame.display.set_mode((300, 200))
-    self.pygame.display.set_caption("Panic at the pizzeria")
-    
+class ConfirmExit():
+    def __init__(self, ai_setting, screen_game):
+        self.ai_setting = ai_setting
+        self.screen_game = screen_game
+        # Параметры текста для вопроса подтверждения выхода.
+        text = "Вы хотите выйти из игры?"
+        self.help_text = pygame.font.SysFont('Serif', 35)
+        self.text_screen = self.help_text.render(text,1, (0, 0, 0))
+        self.text_rect = self.text_screen.get_rect()
+        self.screen_rect = screen_game.get_rect()
+        self.text_rect.x = (140)
+        self.text_rect.y = (200)
+        
+    def blitme_confirm(self):
+        self.screen_game.blit(self.text_screen, self.text_rect)
