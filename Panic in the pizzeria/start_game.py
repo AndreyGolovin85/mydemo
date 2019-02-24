861d7283adf2389145804be58050d2aab356564e
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2019-02-24 21:31:35 +0300

    Update start_game.py

--- Panic in the pizzeria/start_game.py
+++ Panic in the pizzeria/start_game.py
@@ -13,6 +13,7 @@ from game_stats import GameStats
 from scoreboard import ScoreBoard
 from numbers_pizza import NumbersPizza
 from help_game import Help
+from confirm_exit import ConfirmExit
 import game_functions as gf
 
 # Инициализация экрана и создание игры.
@@ -22,11 +23,13 @@ def rungame():
     screen_game = pygame.display.set_mode(
                     (ai_setting.game_width, ai_setting.game_height))
     pygame.display.set_caption("Panic in the pizzeria")
-    # Создание кнопок Play, Help, Quit. (Убрать кнопки.)
+    # Создание кнопок Play, Help, Quit, Yes, No.
     play_button = Button(ai_setting, screen_game, "Play")
     help_button = Button(ai_setting, screen_game, "Help")
     quit_button = Button(ai_setting, screen_game, "Exit")
-    back = Button(ai_setting, screen_game, "Back")
+    back_button = Button(ai_setting, screen_game, "Back")
+    yes_button = Button(ai_setting, screen_game, "Yes")
+    no_button = Button(ai_setting, screen_game, "No")
     # Создание сковороды.
     pan = Pan(screen_game, ai_setting)
     # Создание пиццы.
@@ -43,6 +46,9 @@ def rungame():
     numberspizza = NumbersPizza(ai_setting, screen_game, stats)
     # Вывод справки.
     help_g = Help(ai_setting, screen_game)
+    # Подтверждение выхода из игры.
+    confirm_exit = ConfirmExit(ai_setting, screen_game)
+    mouse_x, mouse_y = pygame.mouse.get_pos()
     
     """Основной цикл игры."""
     while True:
@@ -51,6 +57,7 @@ def rungame():
         gf.update_screen(screen_game,ai_setting, pizzas,
                          pan, pans, pizza, stats, sb, chef,
                          numberspizza, play_button, help_button,
-                         quit_button, help_g, back)
+                         quit_button, help_g, back_button, confirm_exit,
+                         mouse_x, mouse_y, yes_button, no_button)
         
 rungame()
